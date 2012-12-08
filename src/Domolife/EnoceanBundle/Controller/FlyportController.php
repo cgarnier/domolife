c7034c4312fc8b385527f65ff0383e79f0aec6a0
Clément Garnier <clement@laptop-clement>
2012-12-08 10:27:25 +0100

    [master] add device entity

--- src/Domolife/EnoceanBundle/Controller/FlyportController.php
+++ src/Domolife/EnoceanBundle/Controller/FlyportController.php
@@ -7,10 +7,28 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
-class FlyportController extends Controller
-{
-    public function getdataAction($data, Request $request)
-    {
+use Domolife\EnoceanBundle\Entity\Device;
+class FlyportController extends Controller {
+
+    public function getdataAction($data, Request $request) {
+        $em = $this->get('doctrine')->getEntityManager();
+
+        if ($data){
+            $data = json_decode($data);
+            $rep = $em->getRepository("DomolifeEnoceanBundle:Device");
+            if(is_array($res = $rep->find($data->ID))){
+                
+            }
+            else{
+                $device = new Device();
+                $device->id = $data->ID;
+                $device->type = $data->type;
+                $em->persist();
+                $em->flush();
+            }
+        }
+
         return new Response('ok', 200);
     }
+
 }
